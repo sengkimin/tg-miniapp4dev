90e10bf92cc5001e72b2b5a45c81866079585cd0
lynakimhay <nakimhay.ly@institute.pse.ngo>
2025-03-03 16:52:54 +0700

    80%

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -2,6 +2,7 @@ import { JSX } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import ProfilePage from "../pages/ProfilePage";
 import Layout from "../pages/Layout";
+import BranchesPage from "../pages/pageBranch";
 
 export interface AppRoute {
   path?: string;
@@ -12,6 +13,7 @@ export interface AppRoute {
 
 export const webRoutes = {
   home: "/",
+  branch :"/branch"
   // discover: "/discover",
 };
 
@@ -27,7 +29,7 @@ const appRoutes: AppRoute[] = [
     element: <Layout />,
     children: [
       { path: webRoutes.home, element: <ProfilePage /> },
-      // { path: webRoutes.discover, element: <DiscoverPage /> },
+      { path: webRoutes.branch, element: <BranchesPage /> },
     ],
   },
   {
